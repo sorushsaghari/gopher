37063a869a9e95fa71d454bf6050582cc1ec352c
sorush saghari <itnog.hma@gmail.com>
2019-05-16 23:38:34 +0430

    making user controller

--- controllers/users.go
+++ controllers/users.go
@@ -1,3 +1,16 @@
 package controllers
 
+import (
+	"../models"
+)
+
+type UserController struct {
+	us models.UserService
+}
+
+func NewUserController(us models.UserService) *UserController{
+	return  &UserController{
+		us: us,
+	}
+}
 
