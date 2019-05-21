5d200b28f84967b8bbc2c07850d3081a7dfe3647
sorush saghari <itnog.hma@gmail.com>
2019-05-21 17:24:45 +0430

    base retriving and crating data for user service

--- models/users.go
+++ models/users.go
@@ -1,8 +1,16 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+	"fmt"
+)
 
 type UserDb interface {
+	FindById(id uint) (*User, error)
+	Insert(user *User) error
+	FindByEmail(email string) (*User, error)
+	Find(field, value string) (*User, error)
 }
 
 type UserService interface {
@@ -23,4 +31,82 @@ type User struct {
 
 func NewUserService(db *gorm.DB) *userService {
 	return &userService{db: db}
+}
+
+
+
+func (us *userService) Insert(user *User) error {
+	password, err := hashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+	user.Password = password
+	return us.db.Create(user).Error
+}
+
+func (us* userService) Find(field, value string) (*User, error){
+	var user User
+	err := us.db.Where(fmt.Sprintf("%s = ?", field), value).Find(&user).Error
+	fmt.Println(user)
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+func (us *userService) FindById(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = $1", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+func (us* userService) FindByEmail(email string) (*User, error)  {
+	var user User
+	fmt.Println(email)
+	err := us.db.Where("email = ?", email).Find(&user).Error
+	fmt.Println(user)
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+func (us* userService) Authenticate(email string, password string) (bool, error) {
+	u, err := us.Find("email", email)
+	fmt.Println(u)
+	switch err {
+	case nil:
+		return checkPasswordHash(password, u.Password), nil
+	case gorm.ErrRecordNotFound:
+		fmt.Println("sik")
+		return false, ErrNotFound
+	default:
+		fmt.Println("pashm")
+		return false, err
+	}
+}
+
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
